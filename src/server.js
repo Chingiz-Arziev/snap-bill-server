ff76416d9e5622182c5177a7cbc7e8cf520f04d4
chingiz-arziev <chingiz.arziev@gmail.com>
2024-12-30 08:17:59 +0600

    Fix: update ip

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const { server } = require("./app")
 const { PORT } = require("./config/env")
 
-server.listen(PORT, "34.139.139.200", () => {
-  console.log(`Сервер запущен на http://34.139.139.200:${PORT}`)
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Сервер запущен на http://0.0.0.0:${PORT}`)
 })
