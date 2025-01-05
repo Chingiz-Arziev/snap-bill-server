8c01f448d2d7d0fa9576552f722c95f7ddbecd61
chingiz-arziev <chingiz.arziev@gmail.com>
2025-01-05 19:28:14 +0600

    update: send access token to api

--- src/services/visionService.js
+++ src/services/visionService.js
@@ -1,30 +1,15 @@
 const vision = require("@google-cloud/vision")
 const fs = require("fs")
-const {
-  GOOGLE_APPLICATION_CREDENTIALS,
-  OPENAI_API_KEY,
-} = require("../config/env")
+const { getAccessToken } = require("../../keys/pay-me-445906-53799612402b.json")
 
-// Проверка наличия переменной окружения
-if (!GOOGLE_APPLICATION_CREDENTIALS) {
-  throw new Error("GOOGLE_APPLICATION_CREDENTIALS is not defined")
-}
-
-// Проверка доступа к файлу с учетными данными
-try {
-  const credentials = fs.readFileSync(GOOGLE_APPLICATION_CREDENTIALS, "utf8")
-  console.log("Credentials loaded successfully")
-} catch (error) {
-  console.error("Error loading credentials:", error.message)
-  throw error
-}
-
-const client = new vision.ImageAnnotatorClient({
-  keyFilename: GOOGLE_APPLICATION_CREDENTIALS,
-})
+const client = new vision.ImageAnnotatorClient()
 
 const detectText = async (filePath) => {
-  const [result] = await client.textDetection(filePath)
+  const accessToken = await getAccessToken()
+  const [result] = await client.textDetection({
+    image: { source: { filename: filePath } },
+    auth: accessToken,
+  })
   return result.textAnnotations[0]?.description || "Текст не найден"
 }
 
