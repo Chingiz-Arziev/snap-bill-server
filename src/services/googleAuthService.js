42c7b9209b7083962b1bd40f0c3bfe86a3f11348
chingiz-arziev <chingiz.arziev@gmail.com>
2025-01-05 18:47:11 +0600

    Fix

--- src/services/googleAuthService.js
+++ src/services/googleAuthService.js
@@ -1,7 +1,11 @@
 const { GoogleAuth } = require("google-auth-library")
+const path = require("path")
 
 // Путь к JSON файлу с учетными данными сервисного аккаунта
-const KEYFILEPATH = "../../keys/pay-me-445906-20376f17a821.json"
+const KEYFILEPATH = path.resolve(
+  __dirname,
+  "../../keys/pay-me-445906-20376f17a821.json"
+)
 
 // Создание экземпляра GoogleAuth
 const auth = new GoogleAuth({
