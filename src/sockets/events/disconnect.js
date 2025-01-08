814bbe72ea0cad6d540b300c7184bdde7faef740
chingiz-arziev <chingiz.arziev@gmail.com>
2025-01-09 00:29:15 +0600

    Fix: Add condition to disconnected users & room issue

--- src/sockets/events/disconnect.js
+++ src/sockets/events/disconnect.js
@@ -3,14 +3,22 @@ const rooms = require("./rooms")
 const disconnect = (io, socket) => {
   socket.on("disconnect", () => {
     console.log(`Пользователь ${socket.id} отключился`)
-    for (const roomId in rooms) {
-      const userIndex = rooms[roomId].users.findIndex(
-        (user) => user.id === socket.id
-      )
 
-      if (userIndex !== -1) {
-        rooms[roomId].users.splice(userIndex, 1)
-        io.to(roomId).emit("userJoined", { users: rooms[roomId].users })
+    for (const roomId in rooms) {
+      if (rooms[roomId] && Array.isArray(rooms[roomId].users)) {
+        const userIndex = rooms[roomId].users.findIndex(
+          (user) => user.id === socket.id
+        )
+
+        if (userIndex !== -1) {
+          rooms[roomId].users.splice(userIndex, 1)
+
+          io.to(roomId).emit("userJoined", { users: rooms[roomId].users })
+
+          if (rooms[roomId].users.length === 0) {
+            delete rooms[roomId]
+          }
+        }
       }
     }
   })
